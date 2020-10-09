c4d096bc73863c8d26df1a23bdc41c2784bd6e1a
Natacha <natacha.har@onja.org>
2020-10-09 16:30:09 +0300

    Fixed buttons

--- component/App.js
+++ component/App.js
@@ -8,76 +8,130 @@ function App() {
     return (
         <main style={h1Style}>
             <h1 style={h1Style}>Buttons</h1>
-            <Buttons
-                const buttons = {{
-                    type: "<button />",
-                    textContent: "Default",
-                    className: "button",
-                }}
-            />
-            <Buttons 
-                const buttons = {{
+            <Buttons variant= "default"  type="<button />" name="Default"/>
+            <Buttons variant= "outline"  type="<button variant: online />" name="Default"/>
+            <Buttons variant= "text"  type="<button variant: text" name="Default"/>
+            <Buttons variant= "disableShadow"  type="<button disableShadow/>" name="Default"/>
+            <Buttons variant= "disable"  type="<button disable/>" name="Default"/>
+            <Buttons variant= "textDisable"  type="button variant: text, disable" name="Default"/>
+            <Buttons variant= "startIcon"  type="<button startIcon: local_grocery_store />" name="Default"/>
+            <Buttons variant= "endIcon"  type="<button endIcon: local_grocery_store />" name="Default"/>
+            <Buttons size= "sm"  type="<button size: sm />" name="Default"/>
+            <Buttons size= "md"  type="<button size: md />" name="Default"/>
+            <Buttons size= "lg"  type="<button size: lg />" name="Default"/>
+            <Buttons color= "default"  type="<button color: default/>" name="Default"/>
+            <Buttons color= "primary"  type="<button color: default/>" name="Primary"/>
+            <Buttons color= "secondary"  type="<button color: default/>" name="Secondary"/>
+            <Buttons color= "danger"  type="<button color: default/>" name="Danger"/>
+
+            {/* <Buttons
+                const buttons={{
                     type: "$:hover, $:focus",
                     textContent: "Default",
-                    className: "button"
+                    classtype: "buttonVariantOuline",
                 }}
             />
             <Buttons
-                const buttons = {{
-                    type: "<button variant: outline/>",
-                    textContent: "Default",
-                    className: "buttonVariantOuline",
-                }}
-            />
-            <Buttons
-                const buttons = {{
-                    type: "$:hover, $:focus",
-                    textContent: "Default",
-                    className: "buttonVariantOuline",
-                }}
-            />
-            <Buttons
-                const buttons = {{
+                const buttons={{
                     type: "<button variant: text />",
                     textContent: "Default",
                     className: "buttonVariantText"
                 }}
             />
             <Buttons
-                const buttons = {{
+                const buttons={{
                     type: "$:hover, $:focus",
                     textContent: "Default",
                     className: "buttonVariantText"
                 }}
             />
             <Buttons
-                const buttons = {{
+                const buttons={{
                     type: "<button disableShadow/>",
                     textContent: "Default",
                     className: "buttonDesableShadow"
                 }}
             />
             <Buttons
-                const buttons = {{
+                const buttons={{
                     type: "<button disable/>",
-                    name: "Default",
-                    className: "buttonDisable",
+                    textContent: "Default",
+                    className: "buttonDisable"
                 }}
             />
             <Buttons
-                const buttons = {{
-                    type: "",
-                    name: "Default",
-                    className: "",
+                const buttons={{
+                    type: "<button variant: text, disable",
+                    textContent: "Default",
+                    className: "variantTextDisable"
                 }}
             />
             <Buttons
-                const buttons = {{
-                    type: "",
-                    name: "Default",
-                    className: "",
+                const buttons={{
+                    type: "<button startIcon: local_grocery_store>",
+                    textContent: "Default",
+                    className: "startIcon"
                 }}
             />
+            <Buttons
+                const buttons={{
+                    type: "<button endIcon: local_grocery_store/>",
+                    textContent: "Default",
+                    className: "endIcon"
+                }}
+            />
+
+            <Buttons
+                const buttons={{
+                    type: "<button size: sm />",
+                    textContent: "Default",
+                    className: "btnSizeSm"
+                }}
+            />
+            <Buttons
+                const buttons={{
+                    type: "<button size: md />",
+                    textContent: "Default",
+                    className: "btnSizeMd"
+                }}
+            />
+            <Buttons
+                const buttons={{
+                    type: "<button size: lg />",
+                    textContent: "Default",
+                    className: "btnSizelg"
+                }}
+            />
+            <Buttons
+                const buttons={{
+                    type: "<button color: default/>",
+                    textContent: "Default",
+                    className: "colorDefault"
+                }}
+            />
+            <Buttons
+                const buttons={{
+                    type: "<button color: primary />",
+                    textContent: "Primary",
+                    className: "colorPrimary"
+                }}
+            />
+            
+            <Buttons
+                const buttons={{
+                    type: "<button color: secondary />",
+                    textContent: "Secondary",
+                    className: "colorSecondary"
+                }}
+            />
+
+            <Buttons
+                const buttons={{
+                    type: "<button color: danger />",
+                    textContent: "Danger",
+                    className: "colorDanger"
+                }}
+            /> */}
         </main>
     )
 }
--- component/Buttons.js
+++ component/Buttons.js
@@ -1,11 +1,70 @@
 import React from "react";
 
-
 function Buttons(props) {
+    let buttonStyle;
+        if(props.variant == "default") {
+            buttonStyle = "button";
+        }
+
+        if(props.variant == "outline") {
+            buttonStyle = "buttonVariantOuline";
+        }
+
+        if(props.variant == "text") {
+            buttonStyle = "buttonVariantText"
+        }
+
+        if(props.variant == "disableShadow") {
+            buttonStyle = "buttonDesableShadow";
+        }
+
+        if(props.variant == "disable") {
+            buttonStyle = "buttonDisable"
+        }
+
+        if(props.variant == "textDisable") {
+            buttonStyle = "variantTextDisable"
+        }
+
+        if(props.variant == "startIcon") {
+            buttonStyle = "startIcon"
+        }
+
+        if(props.variant == "endIcon") {
+            buttonStyle = "endIcon"
+        }
+
+        if(props.size == "sm") {
+            buttonStyle = "btnSizeSm"
+        }
+
+        if(props.size == "md") {
+            buttonStyle = "btnSizeMd"
+        }
+
+        if(props.size == "lg") {
+            buttonStyle = "btnSizelg"
+        }
+
+        if(props.color == "default") {
+            buttonStyle = "colorDefault"
+        }
+
+        if(props.color == "primary") {
+            buttonStyle = "colorPrimary"
+        }
+
+        if(props.color == "secondary") {
+            buttonStyle = "colorSecondary"
+        }
+
+        if(props.color == "danger") {
+            buttonStyle = "colorDanger"
+        }
     return (
         <div>
-            <p className= "p">{props.buttons.type}</p>
-            <button className={props.buttons.className}>{props.buttons.textContent}</button>
+            <p className= "p">{props.type}</p>
+            <button className={buttonStyle}>{props.name}</button>
         </div>
     )
 }
