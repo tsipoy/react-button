46550779a11ef29e6c008c3917d3bf104025ba20
Natacha <natacha.har@onja.org>
2020-10-13 09:43:51 +0300

    Added dist

--- component/Buttons.js
+++ component/Buttons.js
@@ -2,70 +2,76 @@ import React from "react";
 
 function Buttons(props) {
     let buttonStyle;
-        if(props.variant == "default") {
-            buttonStyle = "button";
-        } else if(props.variant == "outline") {
-            buttonStyle = "buttonVariantOuline";
-        } 
+    if (props.variant == "default") {
+        buttonStyle = "button";
         
-        else if(props.variant == "text") {
-            buttonStyle = "buttonVariantText";
-        } 
-        
-        else if(props.variant == "disableShadow") {
-            buttonStyle = "buttonDesableShadow";
-        } 
-        
-        else if(props.variant == "disable") {
-            buttonStyle = "buttonDisable";
-        } 
-        
-        else if(props.variant == "textDisable") {
-            buttonStyle = "variantTextDisable";
-        } 
-        
-        else if(props.variant == "startIcon") {
-            buttonStyle = "startIcon"
-        }
+    } else if (props.variant == "outline") {
+        buttonStyle = "buttonVariantOuline";
+    }
 
-        else if(props.variant == "endIcon") {
-            buttonStyle = "endIcon"
-        }
+    else if (props.variant == "text") {
+        buttonStyle = "buttonVariantText";
+    }
 
-        else if(props.size == "sm") {
-            buttonStyle = "btnSizeSm"
-        }
+    else if (props.variant == "disableShadow") {
+        buttonStyle = "buttonDesableShadow";
+    }
 
-        else if(props.size == "md") {
-            buttonStyle = "btnSizeMd"
-        }
+    else if (props.variant == "disable") {
+        buttonStyle = "buttonDisable";
+    }
 
-        else if(props.size == "lg") {
-            buttonStyle = "btnSizelg"
-        }
+    else if (props.variant == "textDisable") {
+        buttonStyle = "variantTextDisable";
+    }
 
-        else if(props.color == "default") {
-            buttonStyle = "colorDefault"
-        }
+    else if (props.variant == "startIcon") {
+        buttonStyle = "startIcon"
+    }
 
-        else if(props.color == "primary") {
-            buttonStyle = "colorPrimary"
-        }
+    else if (props.variant == "endIcon") {
+        buttonStyle = "endIcon"
+    }
 
-        else if(props.color == "secondary") {
-            buttonStyle = "colorSecondary"
-        }
+    else if (props.size == "sm") {
+        buttonStyle = "btnSizeSm"
+    }
 
-        else if(props.color == "danger") {
-            buttonStyle = "colorDanger lastBtn"
-        }
+    else if (props.size == "md") {
+        buttonStyle = "btnSizeMd"
+    }
 
-        else {
-            buttonStyle = "buttonVariantOuline";
-        }
+    else if (props.size == "lg") {
+        buttonStyle = "btnSizelg"
+    }
+
+    else if (props.color == "default") {
+        buttonStyle = "colorDefault"
+    }
+
+    else if (props.color == "primary") {
+        buttonStyle = "colorPrimary"
+    }
+
+    else if (props.color == "secondary") {
+        buttonStyle = "colorSecondary"
+    }
+
+    else if (props.color == "danger") {
+        buttonStyle = "colorDanger lastBtn"
+    }
+
+    else {
+        buttonStyle = "buttonVariantOuline";
+    }
+
+    let divClass;
+    if (props.color == "danger") {
+        divClass = "danger";
+    }
     return (
-        <div>
-            <p className= "p">{props.type}</p>
+        <div className={divClass}>
+            <p className="p">{props.type}</p>
             <button className={buttonStyle}>{props.name}</button>
         </div>
     )
